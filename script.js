96a4f66aac60977db954a7082840de3ebd1251b9
Ruzeen <ruzeenrezvie@gmail.com>
2025-11-07 10:17:46 -0800

    test 3

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
-const urlText = document.getElementById('url-text');
+const urlText = document.getElementById("url-text");
 
-const qrcode = new QRCode(document.getElementById('qrcode'));
+const qrcode = new QRCode(document.getElementById("qrcode"));
 
 const makeCode = () => {
-	if (!urlText.value) {
-		urlText.placeholder = 'Enter a URL';
-	}
+  if (!urlText.value) {
+    urlText.placeholder = "Enter a URL";
+  }
 
-	qrcode.makeCode(urlText.value);
-}
+  qrcode.makeCode(urlText.value);
+};
 
 makeCode();
 
 // Add event listener for the "blur" event
-urlText.addEventListener('blur', () => {
-    makeCode();
+urlText.addEventListener("blur", () => {
+  makeCode();
 });
 
 // Add event listener for the "keydown" event
 urlText.addEventListener("keydown", function (e) {
-    if (e.key === 'Enter') {  // Check if the Enter key was pressed
-        makeCode();
-    }
+  if (e.key === "Enter") {
+    // Check if the Enter key was pressed
+    makeCode();
+  }
 });
 
-const downloadBtn = document.getElementById('download-btn');
+const downloadBtn = document.getElementById("download-btn");
 
 // Save QR code
-downloadBtn.addEventListener('click', () => {
-    const img = document.querySelector('#qrcode img');
-    const a = document.createElement('a');
-    a.href = img.src; 
-    a.download = 'qrcode.png';
+downloadBtn.addEventListener("click", () => {
+  const img = document.querySelector("#qrcode img");
+  const a = document.createElement("a");
+  a.href = img.src;
+  a.download = "qrcode.png";
 
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
 });
+
+// test 10:18
